27614429089792e55038c32df81539d4028dbef8
Andreas Schmidt <andreas.schmidt@cassini.de>
2015-01-21 13:04:00 +0100

    initial add

---
+++ 02_docker_tls/scripts.d/01_create_ca.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+# --
+# create a ca structure
+
+# must run as root
+if [[ `id -u` != 0 ]]; then
+	echo ERROR must run as root.
+	exit 1
+fi
+
+CA_PATH=/etc/docker-tls
+
+if [[ ! -d $CA_PATH ]]; then
+	mkdir $CA_PATH
+	chmod 0700 $CA_PATH
+fi
+
+# create file/dir structure
+( umask o-rwx,g-wx,u+rw; \
+	mkdir $CA_PATH/certs $CA_PATH/private $CA_PATH/csr >/dev/null 2>&1
+	[[ -f $CA_PATH/serial ]] || echo '100001' >$CA_PATH/serial
+	touch $CA_PATH/certindex.txt
+)
+
+# generate key, use passphase file
+# to encrypt output
+if [[ ! -f $CA_PATH/private/ca-key.pem ]]; then
+	( umask o-rwx,g-wx,u+rw; \
+		openssl req -new \
+			-x509 \
+			-keyout $CA_PATH/private/cakey.pem \
+			-passout file:./passphrase-file \
+			-out $CA_PATH/cacert.pem \
+			-config ./openssl.cnf \
+			-batch
+	)
+fi
+
