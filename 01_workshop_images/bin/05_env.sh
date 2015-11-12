0738d1fcf7f64dece2baf32ec1a95af159cf9f1c
Dustin Huptas <dustin@huptas.net>
2015-11-12 15:12:36 +0100

    enable pw auth and sudo for demo user

--- 01_workshop_images/bin/05_env.sh
+++ 01_workshop_images/bin/05_env.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
 sudo sh -c "echo 'demo:demo' | chpasswd"
+sudo adduser demo sudo
 sudo sh -c "echo 'LANG=en_US.UTF-8' >/etc/locale.conf"
 sudo sh -c "echo 'LC_MESSAGES=C' >/etc/locale.conf"
 sudo /usr/bin/localectl set-locale LANG=en_US.UTF-8
+sudo sh -c "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
