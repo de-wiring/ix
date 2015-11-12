eda28ad34fefaaff825fa0e0363b0ae04570df3a
Andreas Schmidt <andreas.schmidt@cassini.de>
2015-11-12 11:17:19 +0100

    updated

--- 046_apparmor_debian/provision.d/01_os.sh
+++ 046_apparmor_debian/provision.d/01_os.sh
@@ -2,5 +2,5 @@
 echo 'deb http://http.debian.net/debian jessie-backports main' >>/etc/apt/sources.list
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y vim git curl
+apt-get install -y vim git curl ruby rubygems-integration
 
