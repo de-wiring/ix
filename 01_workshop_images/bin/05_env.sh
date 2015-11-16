f2b504d963822a497fe57b779dde8ec1d8dff9b8
Dustin Huptas <dustin@huptas.net>
2015-11-16 11:56:54 +0100

    fix sudo rights for demo user in fedora image

--- 01_workshop_images/bin/05_env.sh
+++ 01_workshop_images/bin/05_env.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 sudo sh -c "echo 'demo:demo' | chpasswd"
-sudo adduser demo sudo
 sudo sh -c "echo 'LANG=en_US.UTF-8' >/etc/locale.conf"
 sudo sh -c "echo 'LC_MESSAGES=C' >/etc/locale.conf"
 sudo /usr/bin/localectl set-locale LANG=en_US.UTF-8
