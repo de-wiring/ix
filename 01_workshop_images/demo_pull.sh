b500776833a2b166de889a0f45d4e4f144a6813f
Dustin Huptas <dustin@huptas.net>
2015-11-17 11:24:54 +0100

    fix lynis permissions

--- 01_workshop_images/demo_pull.sh
+++ 01_workshop_images/demo_pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function clone_or_pull_github() {
-	echo Updating $1/$2
+	echo Updating "$1"/"$2"
 
 	OWNER="$1"
 	REPO="$2"
@@ -10,19 +10,24 @@ function clone_or_pull_github() {
 	P=${TARGET}/${REPO}
 
 	if [[ ! -d ${TARGET} ]]; then
-		mkdir ${TARGET}
+		mkdir "${TARGET}"
 	fi
 
 	if [[ ! -d $P ]]; then
 		URL=https://github.com/$OWNER/$REPO
-		( cd $TARGET; git clone -q $URL >/dev/null)
+		( cd "$TARGET" || exit_on_error "ERROR: directory does not exist. Exiting."; git clone -q "$URL" >/dev/null)
 	else
-		( cd $P; git pull -r >/dev/null)
+		( cd "$P" || exit_on_error "ERROR: directory does not exist. Exiting."; git pull -r >/dev/null)
 	fi
 }
 
+function exit_on_error() {
+    echo "$1"
+    exit 1
+}
+
 if id -u demo >/dev/null 2>&1; then
-  cd /home/demo
+  cd /home/demo || exit_on_error "ERROR: directory does not exist. Exiting."
   clone_or_pull_github de-wiring tests-docker-hardening .
   clone_or_pull_github de-wiring containerspec .
   clone_or_pull_github de-wiring ix .
@@ -31,11 +36,13 @@ if id -u demo >/dev/null 2>&1; then
   clone_or_pull_github docker docker third-party
   clone_or_pull_github docker docker-bench-security third-party
   clone_or_pull_github cisofy lynis third-party
+  sudo chown -R root. /home/demo/third-party/lynis
+  sudo chmod 640 /home/demo/third-party/lynis/include/consts
+  sudo chmod 640 /home/demo/third-party/lynis/include/functions
 
   # extra docker 1.8.3
-  ( cd /home/demo/third-party/docker;  git fetch --tags; git checkout v1.8.3 )
+  ( cd /home/demo/third-party/docker || exit_on_error "ERROR: directory does not exist. Exiting.";  git fetch --tags; git checkout v1.8.3 )
 
 else
-  echo "Error: user demo does not exist."
-  exit 1
+  exit_on_error "ERROR: user demo does not exist. Exiting."
 fi
