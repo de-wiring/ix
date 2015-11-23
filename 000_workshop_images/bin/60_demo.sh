750ee98f8eb6857f183c3bd82b63cda0ceecf844
Andreas Schmidt <andreas.schmidt@cassini.de>
2015-11-23 09:16:07 +0100

    fix

--- 000_workshop_images/bin/60_demo.sh
+++ 000_workshop_images/bin/60_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ ! -f /home/demo/demo_pull.sh ]; then
-	sudo su - demo -c "cd /home/demo && curl -sSL https://raw.githubusercontent.com/de-wiring/ix/master/01_workshop_images/demo_pull.sh | bash"
+	sudo su - demo -c "cd /home/demo && curl -sSL https://raw.githubusercontent.com/de-wiring/ix/master/000_workshop_images/demo_pull.sh | bash"
 fi
