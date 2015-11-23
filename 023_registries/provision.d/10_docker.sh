41dcb02c750c72fce0abe0ca65f579b30755bcca
Dustin Huptas <dustin@huptas.net>
2015-11-23 15:29:31 +0100

    fix

--- 023_registries/provision.d/10_docker.sh
+++ 023_registries/provision.d/10_docker.sh
@@ -5,7 +5,4 @@ sudo /bin/systemctl enable docker || sudo chkconfig -add docker
 sudo /bin/systemctl start docker
 sudo /usr/bin/docker pull dewiring/trustit
 
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-\"$(uname -s)\"-\"$(uname -m)\" > /usr/local/bin/docker-compose"
-chmod +x /usr/local/bin/docker-compose
-
 sudo usermod -aG docker demo
--- 023_registries/provision.d/20_notary.sh
+++ 023_registries/provision.d/20_notary.sh
@@ -2,16 +2,20 @@
 
 sudo sh -c 'echo "127.0.0.1 notaryserver" >> /etc/hosts'
 sudo sh -c 'echo "127.0.0.1 sandboxregistry" >> /etc/hosts'
+
 mkdir notarysandbox
 cd notarysandbox || exit
+
 mkdir notarytest
 cd notarytest || exit
 cp /tmp/Dockerfile .
 docker build -t notarysandbox .
+
 cd .. || exit
 git clone -b trust-sandbox https://github.com/docker/notary.git
-git clone https://github.com/docker/distribution.git
 cd notary || exit
 docker-compose build
+
+git clone https://github.com/docker/distribution.git
 cd ../distribution || exit
 docker build -t sandboxregistry .
